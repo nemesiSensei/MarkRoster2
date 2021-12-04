0aebbebd49925ae6e0545bcf0a8e46a486567f8a
Sebastián Méndez Montealegre <smendez67@misena.edu.co>
2021-12-04 12:08:32 -0500

    ya reconoce el horario del sistema, falta es que lo envieee ahhh

--- src/main/java/controlador/horariosDAO.java
+++ src/main/java/controlador/horariosDAO.java
@@ -9,10 +9,11 @@ import horarios.horario;
 import java.util.Calendar;
 
 public class horariosDAO {
-	getters r = new getters();
+	
 	horario fecha = new horario();
 	Connection con;
 	ResultSet rs;
+	
 	PreparedStatement ps;
 	String sql;
 	conexion c= new conexion();
@@ -20,15 +21,16 @@ public class horariosDAO {
 	int id;
 	public int  registrar(getters r) throws SQLException { //  este va a ser el metodo para hacer el registro de horario.
 		System.out.println("Entro a la sentencia preparada  ");
+		
 		sql="INSERT INTO horario (Hora_de_entrada, Hora_de_salida, Horainicio, horafin, id_empleado) VALUES ('?','?','?','?','?')";
 		
 		try {
 			con=c.getConnection(); // opening the connection to database 
 			ps=con.prepareStatement(sql); // prepare that sentence 
-			ps.setString(1,fecha.fechaactual());
-			ps.setString(2, fecha.fechaactual());
-			ps.setString(3, fecha.fechaactual());
-			ps.setString(4, fecha.fechaactual());
+			ps.setString(1,horario.horaactual());
+			ps.setString(2, horario.horaactual());
+			ps.setString(3, horario.horaactual());
+			ps.setString(4, horario.horaactual());
 			ps.setInt(5, r.getIdempresa());
 			System.out.println("Se logro registrar el horario, uwuuuuuuuuuuuuu");
 		
