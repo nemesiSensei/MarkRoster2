60692fe20dfd589e444b30872dfd17e2c53adf65
Sebastián Méndez Montealegre <smendez67@misena.edu.co>
2021-12-12 16:26:31 -0500

    ya funciona lo de los horarios, obviamente falta detalles y el calculo de horas

--- src/main/java/modelo/horariosDAO.java
+++ src/main/java/modelo/horariosDAO.java
@@ -27,38 +27,11 @@ public class horariosDAO {
 	getters r = new getters();
 	
 	
-	public int  registrar(horarioVo hv) throws SQLException { //  este va a ser el metodo para hacer el registro de horario.
-		System.out.println("Entro a la sentencia preparada  ");
-		
-		sql="INSERT INTO horario (Hora_de_entrada, Hora_de_salida, Horainicio, horafin, id_empleado) VALUES (?,?,?,?,?)";
-		
-		
-		try {
-			con=c.getConnection(); // opening the connection to database 
-			ps=con.prepareStatement(sql); // prepare that sentence 
-			ps.setString(1,hv.getFechaentrada());
-			ps.setString(2, hv.getFechasalida());
-			ps.setString(3,hv.getFechainicio());
-			ps.setString(4, hv.getFechafin());
-			ps.setInt(5,hv.getIdempleados().getIdempresa());
-			System.out.println("Se logro registrar el horario, uwuuuuuuuuuuuuu");
-			ps.executeUpdate();
-		
-			
-			System.out.println("La fecha actual es: "+horario.horaactual());
-			
-			
-		
-		}catch (Exception e) { 
-			System.out.println("Algo sucedio mal  al registrar la fecha actual"+e.getMessage());
-			ps.close();
-		}		
-		return register;
-	}
+	
 	public int  registrar_hora_inicio(horarioVo hv) throws SQLException { //  este va a ser el metodo para hacer el registro de horario.
 		System.out.println("Entro a la sentencia preparada  ");
 		
-		sql="INSERT INTO horario (Hora_de_entrada, id_empleado) VALUES (?,?)";
+		sql="INSERT INTO horario_entrada (Horario_entrada, id_empleados) VALUES (?,?)";
 		
 		
 		try {
@@ -83,14 +56,14 @@ public class horariosDAO {
 	public int  registrar_hora_salidaAlmuerzo(horarioVo hv) throws SQLException { //  este va a ser el metodo para hacer el registro de horario.
 		System.out.println("Entro a la sentencia preparada  ");
 		
-		sql="INSERT INTO horario (fin_almuerzo,id_empleado) VALUES (?,?)";
+		sql="INSERT INTO salida_almuerzo (id_empleado, fin_almuerzo) VALUES (?,?)";
 		
 		
 		try {
 			con=c.getConnection(); // opening the connection to database 
 			ps=con.prepareStatement(sql); // prepare that sentence 
-			ps.setString(1,hv.getFechasalida());
-			ps.setInt(2,hv.getIdempleados().getIdempresa());
+			ps.setString(2,hv.getFechasalida());
+			ps.setInt(1,hv.getIdempleados().getIdempresa());
 			System.out.println("Se logro registrar el horario, uwuuuuuuuuuuuuu");
 			ps.executeUpdate();
 		
@@ -108,14 +81,14 @@ public class horariosDAO {
 	public int  registrarinicio_almuerzo(horarioVo hv) throws SQLException { //  este va a ser el metodo para hacer el registro de horario.
 		System.out.println("Entro a la sentencia preparada  ");
 		
-		sql="INSERT INTO horario (inicio_almuerzo,id_empleado) VALUES (?,?)";
+		sql="INSERT INTO inicio_almuerzo (id_empleado, id_inicio) VALUES (?,?);";
 		
 		
 		try {
 			con=c.getConnection(); // opening the connection to database 
 			ps=con.prepareStatement(sql); // prepare that sentence 
-			ps.setString(1,hv.getFechainicio());
-			ps.setInt(2,hv.getIdempleados().getIdempresa());
+			ps.setString(2,hv.getFechainicio());
+			ps.setInt(1,hv.getIdempleados().getIdempresa());
 			System.out.println("Se logro registrar el horario, uwuuuuuuuuuuuuu");
 			ps.executeUpdate();
 		
@@ -133,16 +106,17 @@ public class horariosDAO {
 	public int  registrarsalida_laboral(horarioVo hv) throws SQLException { //  este va a ser el metodo para hacer el registro de horario.
 		System.out.println("Entro a la sentencia preparada  ");
 		
-		sql="INSERT INTO horario (Hora_de_salida,id_empleado) VALUES (?,?)";
+		sql="INSERT INTO hora_salida ( id_empleado, Horario_salida) VALUES (?,?);";
 		
 		
 		try {
 			con=c.getConnection(); // opening the connection to database 
 			ps=con.prepareStatement(sql); // prepare that sentence 
-			ps.setString(1,hv.getFechafin());
-			ps.setInt(2,hv.getIdempleados().getIdempresa());
+			ps.setString(2,hv.getFechafin());
+			ps.setInt(1,hv.getIdempleados().getIdempresa());
 			System.out.println("Se logro registrar el horario, uwuuuuuuuuuuuuu");
 			ps.executeUpdate();
+			System.out.print(ps);
 		
 			
 			System.out.println("La fecha actual es: "+horario.horaactual());
